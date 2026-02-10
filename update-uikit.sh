c57f3105859cf6a86d660a62eb5b676f96cde794
Radovan Bast <version-control@radovan.xyz>
2026-02-10 08:17:42 +0100

    update the update script

--- update-uikit.sh
+++ update-uikit.sh
@@ -7,12 +7,15 @@ cd uikit
 git checkout main
 git pull origin main
 
-latest_tag=$(git describe --tags --abbrev=0)
+latest_tag=$(awk -F'"' '/"version":/ {print $4}' package.json)
 
 cd ..
 git add uikit
 
 old_version=$(grep -oP "uikit@(\d+\.\d+\.\d+)" templates/base.html | head -n 1)
 new_version="uikit@${latest_tag#v}"
+
+echo "previous version: ${old_version}"
+echo "new version:      ${new_version}"
 
 sed -i "s/${old_version}/${new_version}/g" templates/base.html
